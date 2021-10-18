81fbd01f0c5dc6e84d5a3e9af5dcab63c48a9434
Hafiz Muhammad Ahmad Naeem <hafizmahmadnaeem@gmail.com>
2021-10-18 19:24:03 +0500

    useStateHook concept

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import React, { useState } from "react";
 function App() {
+  const [numValue, setNumValue] = useState(10);
+
+  const Incre = () => {
+    setNumValue(numValue + 5);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>{numValue}</h1>
+      <button onClick={Incre}>Click Me</button>
     </div>
   );
 }
